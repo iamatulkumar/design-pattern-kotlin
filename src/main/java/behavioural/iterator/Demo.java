1871dd657723227a4d63e90b342b06037fa24b24
Ankit Bansal <ankit.bansal@gojek.com>
2022-06-23 10:44:13 +0530

    [jayesh] - update theory of facade.

--- src/main/java/behavioural/iterator/Demo.java
+++ src/main/java/behavioural/iterator/Demo.java
@@ -3,5 +3,8 @@ package behavioural.iterator;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("This is iterator demo");
+        System.out.println("S1");
+        System.out.println("S2");
+        System.out.println("S3");
     }
 }
