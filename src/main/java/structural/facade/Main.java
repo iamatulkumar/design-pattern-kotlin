cc2d9397e6aa063ff9869256c5306110d3cca38f
jayeshdalal <jayesh.dalal@go-jek.com>
2022-06-23 10:41:38 +0530

    [jayesh] - update theory of facade.

--- src/main/java/structural/facade/Main.java
+++ src/main/java/structural/facade/Main.java
@@ -2,6 +2,26 @@ package structural.facade;
 
 import java.util.Arrays;
 
+/*
+
+Facade Design Pattern
+
+The Facade is a Structural Design Pattern and one of the Gang of Four design patterns.
+The Facade object is used to provide a front-facing interface by masking a more complex underlying system.
+The Facade may provide a limited or dedicated set of functionalities. But, the functionalities Facade provides are mainly required by the client application. So, its more caring as per client needs.
+The primary purpose of the Facade is to hide complexities of a system/subsystem by providing simpler interface to deal with.
+Using Facade is super-easy when we have to deal with a complex system/subsystem having lots of functionalities and different configurations.
+So, Facade hides minor and inner details of any third party library, system or subsystem we should know before we deal with it.
+In Java there are many features like JDBC, JPA, JAX-RS etc. which hides the minor details and provide a simpler interface in form of annotations or easier configuration to deal with.
+Even our computer system's POST (power-on-self-test) procedure which runs at the time we start our system; is a good example of Facade. it checks RAM, CPU, HDD and other connected peripherals before giving control over to operating system.
+Facade introduces additional layer of abstraction via Facade. So, if the sub-system changes, we need to do corresponding changes in the facade layer as well.
+
+
+Facade Pattern Vs Proxy Pattern
+Proxy Pattern is similar to Facade except, it provides same interface as it's service object to make complex objects interchangeable.
+
+
+ */
 public class Main {
 
 	public static void main(String[] args) {
