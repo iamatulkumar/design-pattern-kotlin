ea618f53cc6ce3d20656e22d70e1d05e79ec412b
Ankit Bansal <ankit.bansal@gojek.com>
2022-06-23 10:44:00 +0530

    added sout 2

--- src/main/java/behavioural/iterator/Demo.java
+++ src/main/java/behavioural/iterator/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("This is iterator demo");
         System.out.println("S1");
+        System.out.println("S2");
     }
 }
